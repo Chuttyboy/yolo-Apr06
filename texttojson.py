fc231517c2f6462d04f934d1a7a1367b0aff7ae1
Harry <79959547+Chuttyboy@users.noreply.github.com>
2022-04-06 10:03:28 +0530

    Update texttojson.py

--- texttojson.py
+++ texttojson.py
@@ -7,7 +7,8 @@ cam_details =  pd.read_csv('output.txt', sep=r'(?:,\s*|^)(?:\d+: \d+x\d+|Done[^)
                  header=None, engine='python', names=(None, 'a', 'b', 'date')).iloc[:, 1:]
 
 
-cam_details.to_json('file1.json', orient = "records", date_format = "epoch", double_precision = 10, force_ascii = True, date_unit = "ms", default_handler = None)
+cam_details.to_json('file1.json', orient = "records", date_format = "epoch", 
+                            double_precision = 10, force_ascii = True, date_unit = "ms", default_handler = None)
 
 
 
